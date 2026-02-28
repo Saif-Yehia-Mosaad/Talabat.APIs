11323c522fd7b4db6c2d4cfd77f701d11d8e47da
Hi-Tech <Hi-Tech@DESKTOP-0MDMFGG>
2026-02-28 17:00:32 +0200

    Swagger

--- Talabat.APIs/Program.cs
+++ Talabat.APIs/Program.cs
@@ -1,3 +1,6 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Talabat.APIs
 {
@@ -8,23 +11,24 @@ namespace Talabat.APIs
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+
+            // Swagger
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
